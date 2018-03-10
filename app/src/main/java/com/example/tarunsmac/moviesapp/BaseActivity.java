6faf944ca0ff63db45936dbf61e394f22d9fd349
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2018-03-10 16:13:04 +0530

    integrated activity with ViewModel to fetch api data

---
+++ app/src/main/java/com/example/tarunsmac/moviesapp/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.example.tarunsmac.moviesapp;
+
+import android.arch.lifecycle.LifecycleRegistry;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tarunsmac on 10/03/18.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+}

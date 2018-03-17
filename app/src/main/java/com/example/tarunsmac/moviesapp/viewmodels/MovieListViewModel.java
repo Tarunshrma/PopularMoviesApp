bf7a844284341f2b0f6a79bd381b5cef8e84bd54
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2018-03-17 11:22:58 +0530

    Code cleaning

--- app/src/main/java/com/example/tarunsmac/moviesapp/viewmodels/MovieListViewModel.java
+++ app/src/main/java/com/example/tarunsmac/moviesapp/viewmodels/MovieListViewModel.java
@@ -17,7 +17,7 @@ public class MovieListViewModel extends BaseViewModel {
 
     //Constructor injection for api service.
     public MovieListViewModel(){
-        //TODO: Service should be injected... Construcot injection was not working with Dagger
+        //TODO: Service should be injected... Construcot injection was not working with Dagger and ViewModel
         this.apiService = new MovieServiceImpl();
     }
 
